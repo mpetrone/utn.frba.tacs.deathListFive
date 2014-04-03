2ac801fe0969ed3d407f424489db65c54d8a1dc2
mpetrone <matias.petrone@gmail.com>
2014-04-03 19:33:11 -0300

    primer aproach del proyecto, un servidor simple usar griffy y jersey

---
+++ src/main/java/com/tacs/deathlist/Main.java
@@ -0,0 +1,45 @@
+package com.tacs.deathlist;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Main class.
+ *
+ */
+public class Main {
+    // Base URI en donde vamos a estar escuchando
+    public static final String BASE_URI = "http://localhost:8080/deathlist/";
+
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     * @return Grizzly HTTP server.
+     */
+    public static HttpServer startServer() {
+        //agarra las clases del paquete com.tacs.deathlist e inicia el servidor
+        final ResourceConfig rc = new ResourceConfig().packages("com.tacs.deathlist");
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    /**
+     * Main method.
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        HttpServer server = null;
+        try {
+            server = startServer();
+            System.in.read();
+        } catch (Exception e) {
+            System.out.println("Hubo un problema con el servidor: " + e);
+            if(server != null)
+                server.shutdownNow();
+        }
+    }
+}
+
---
+++ src/test/java/com/tacs/deathlist/ListasTest.java
@@ -0,0 +1,50 @@
+package com.tacs.deathlist;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+public class ListasTest {
+
+    private HttpServer server;
+    private WebTarget target;
+    private Gson gson = new Gson();
+
+    @Before
+    public void setUp() throws Exception {
+        // start the server
+        server = Main.startServer();
+        // create the client
+        Client c = ClientBuilder.newClient();
+        target = c.target(Main.BASE_URI);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        server.shutdownNow();
+    }
+
+
+    @Test
+    public void testGetLists() {
+        String jsonResponse = target.path("lists").request().get(String.class);
+        
+        assertNotNull(jsonResponse);
+        assertTrue(!jsonResponse.isEmpty());
+        List<?> list = gson.fromJson(jsonResponse, List.class);
+        assertEquals(3, list.size());
+    }
+}
