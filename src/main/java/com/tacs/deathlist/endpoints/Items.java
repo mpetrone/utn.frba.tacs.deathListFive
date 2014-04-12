5098e5a5c1e52f25e20468f5e79f8af19fa56cf6
eugeniobruno <eugeniobruno@gmail.com>
2014-04-12 14:15:58 -0300

    Agregados los verbos para agregar items y votar.

---
+++ src/main/java/com/tacs/deathlist/endpoints/Items.java
@@ -0,0 +1,32 @@
+package com.tacs.deathlist.endpoints;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+@Path("item/{itemName}")
+public class Items {
+	
+	@POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createItem(@PathParam("itemName") String itemName) { 
+        System.out.format("Entró a createItem en Item con: %s%n",itemName);
+        return Response.status(Status.CREATED).build();
+    }
+	
+	@PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response voteItem(@PathParam("itemName") String itemName) { 
+        System.out.format("Entró a voteItem en Item con: %s%n",itemName);
+        return Response.status(Status.CREATED).build();
+    }   
+
+}
