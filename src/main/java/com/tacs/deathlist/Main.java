89a117e99e84d766ff8926e70cd6f0b476e0addc
mpetrone <matias.petrone@gmail.com>
2014-04-20 21:21:21 -0300

    implementing log4j

--- src/main/java/com/tacs/deathlist/Main.java
+++ src/main/java/com/tacs/deathlist/Main.java
@@ -1,11 +1,17 @@
 package com.tacs.deathlist;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Properties;
+
+import org.apache.log4j.PropertyConfigurator;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
-
-import java.io.IOException;
-import java.net.URI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 /**
  * Main class.
@@ -14,6 +20,7 @@ import java.net.URI;
 public class Main {
     // Base URI en donde vamos a estar escuchando
     public static final String BASE_URI = "http://localhost:8080/deathlist/";
+    public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
@@ -31,15 +38,25 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
+        configureLog4j();
         HttpServer server = null;
         try {
             server = startServer();
             System.in.read();
         } catch (Exception e) {
-            System.out.println("Hubo un problema con el servidor: " + e);
+            LOGGER.error("Hubo un problema con el servidor: " + e);
             if(server != null)
                 server.shutdownNow();
         }
+    }
+
+    private static void configureLog4j() throws IOException{
+        String log4JPropertyFile = System.getProperty( "user.dir") + "/src/main/resources/com/tacs/deathlist/properties/log4j.properties";
+        Properties p = new Properties();
+        p.load(new FileInputStream(log4JPropertyFile));
+        PropertyConfigurator.configure(p);
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
     }
 }
 
