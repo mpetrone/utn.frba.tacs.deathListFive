0661d94985c29cbca305b3c147ee67df03a47361
javs <javierous@gmail.com>
2014-05-02 23:36:22 -0300

    Agrega logica para borrar items y listas.

--- src/main/resources/com/tacs/deathlist/client/js/app.js
+++ src/main/resources/com/tacs/deathlist/client/js/app.js
@@ -28,10 +28,9 @@ $.ajax({
                // TODO: mostrar en la pagina
                alert('Error en GET lists');
             })
-            .done(function(data) {
-               return data.map(function(list){
+            .done(function(lists) {
+               return lists.forEach(function (list) {
                   list.id = list.nombre;
-                  return list;
                });
             });
       }
@@ -44,9 +43,12 @@ $.ajax({
                // TODO: mostrar en la pagina
                alert('Error en GET lists');
             })
-            .done(function(data) {
-               data.id = data.nombre;
-               return data;
+            .done(function(list) {
+               list.id = list.nombre;
+               list.items.forEach(function (item) {
+                  item.id = item.nombre;
+               });
+               return list;
             });
       }
    });
@@ -61,10 +63,10 @@ $.ajax({
             if (!list.trim()) { return; }
             
             var controller = this;
-            return $.post(API_NAMESPACE + GUEST_PATH + '/lists/' + list)
+            $.post(API_NAMESPACE + GUEST_PATH + '/lists/' + list)
                .done(function () {
                   // limpia el input
-                  controller.set('newList', null);
+                  controller.set('newList', '');
                   
                   // actualiza la lista
                   controller.get('content').unshiftObject({
@@ -72,6 +74,68 @@ $.ajax({
                      nombre: list
                   });
                });
+         },
+         deleteList: function (list) {
+            // TODO: confirmation con bootstrap
+            // TODO: error handling
+            
+            var controller = this;
+            $.ajax({
+               url: API_NAMESPACE + GUEST_PATH + '/lists/' + list.id,
+               type: 'DELETE',
+               dataType: 'html' // la respuesta viene vacia, eso no le gusta al parser de json
+            }).done(function () {
+               var content = controller.get('content');
+               
+               controller.set('content', content.rejectBy('id', list.id));
+            });
+            
+         }
+      }
+   });
+   
+   App.ListController = Ember.ObjectController.extend({
+      actions: {
+         createItem: function() {
+            // TODO: error handling
+            
+            var list = this.get('id');
+            
+            // valor del input
+            var item = this.get('newItem');
+            if (!item.trim()) { return; }
+            
+            var controller = this;
+            $.post(API_NAMESPACE + GUEST_PATH + '/lists/' + list + '/items/' + item)
+               .done(function () {
+                  // limpia el input
+                  controller.set('newItem', '');
+                  
+                  // actualiza la lista
+                  controller.get('content').items.unshiftObject({
+                     id: item,
+                     nombre: item
+                  });
+               });
+         },
+         deleteItem: function (item) {
+            // TODO: confirmation con bootstrap
+            // TODO: error handling
+            
+            var list = this.get('id');
+            
+            var controller = this;
+            $.ajax({
+               url: API_NAMESPACE + GUEST_PATH + '/lists/' + list + '/items/' + item.id,
+               type: 'DELETE',
+               dataType: 'html' // la respuesta viene vacia, eso no le gusta al parser de json
+            }).done(function () {
+               // actualiza la lista
+               var items = controller.get('content.items');
+               
+               controller.set('content.items', items.rejectBy('id', item.id));
+            });
+            
          }
       }
    });
